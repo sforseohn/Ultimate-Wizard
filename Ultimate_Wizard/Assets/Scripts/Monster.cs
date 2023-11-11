9158963ab5c4c7cd6ffc37a512d101442e457883
sforseohn <100199530+sforseohn@users.noreply.github.com>
2023-11-11 13:38:21 +0900

    Feat: Monster Pattern 6, 7 (#6)
    * Feat: Monster Pattern 6
    
    * Feat: Bullet Color Change
    
    * Feat: Monster Pattern 7
    
    * Merge main into Monster_P6_7 (#5)

--- Ultimate_Wizard/Assets/Scripts/Monster.cs
+++ Ultimate_Wizard/Assets/Scripts/Monster.cs
@@ -8,9 +8,6 @@ public class Monster : MonoBehaviour
     private float health = 1000f;
 
     [SerializeField]
-    private float speed = 1f;
-
-    [SerializeField]
     private float delay = 0.2f; // 기준 딜레이
     public float curDelay;        // 현재 딜레이 시간
 
@@ -21,7 +18,17 @@ public class Monster : MonoBehaviour
     private GameObject bullet;
 
     [SerializeField]
+    private GameObject bullet6;
+    [SerializeField]
+    private GameObject bullet7;
+
+    [SerializeField]
     private GameObject player;
+
+    private bool canShoot = true;
+    private float cooldown = 5f; // 5초 동안 공격 X
+
+    private int simulNum = 3;
 
     void Start()
     {
@@ -35,7 +42,8 @@ public class Monster : MonoBehaviour
             Win();
         }
 
-        Attack(3);
+        // TODO: 랜덤 공격 패턴
+        Attack(7);
         Reload();
     }
 
@@ -63,7 +71,22 @@ public class Monster : MonoBehaviour
             case 3:
                 AttackPattern3();
                     break;
-                
+            case 4:
+                AttackPattern1();
+                break;
+            case 5:
+                AttackPattern2();
+                break;
+            case 6:
+                if (canShoot)
+                {
+                    AttackPattern6();
+                    StartCoroutine(ShootCooldown());
+                }
+                break;
+            case 7:
+                AttackPattern7();
+                break;
         }
 
         curDelay = 0;
@@ -72,7 +95,7 @@ public class Monster : MonoBehaviour
     // 몬스터 공격 패턴 1 - 홀수형
     void AttackPattern1()
     {
-        // 랜덤 
+        // TODO: 랜덤 숫자
         int lines = 7;
         float angleDiff = 20f; // 중심으로부터의 각도
 
@@ -104,7 +127,7 @@ public class Monster : MonoBehaviour
 
     void AttackPattern2()
     {
-        // 랜덤 
+        // TODO: 랜덤 숫자
         int lines = 4;
         float angleDiff = 20f; // 중심으로부터의 각도
 
@@ -161,6 +184,70 @@ public class Monster : MonoBehaviour
             bullet.transform.Rotate(rotVec);
         }
     }
+
+    IEnumerator ShootCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(cooldown);
+        canShoot = true;
+    }
+
+
+    void AttackPattern6()
+    {
+        int bulletCount = 20; // 동시에 나오는 총알 수
+
+        for (int i = 0; i < bulletCount; i++)
+        {
+            GameObject b = Instantiate(bullet6, transform.position, transform.rotation);
+            Rigidbody2D rigid = b.GetComponent<Rigidbody2D>();
+
+            // 랜덤 각도 및 속도
+            float angle = Random.Range(-25f, 25f);
+            float speed = Random.Range(3f, 15f);
+
+            // 총알 방향
+            Vector2 direction = Quaternion.Euler(0, 0, angle) * Vector2.right.normalized;
+
+            rigid.AddForce(direction.normalized * speed, ForceMode2D.Impulse);
+        }
+    }
+
+    void AttackPattern7()
+    {
+        int bulletCount = 5; // 동시에 나오는 총알 수
+        
+        CircleCollider2D collider = bullet7.GetComponent<CircleCollider2D>();
+        float bulletWidth = 0;
+
+        if (collider != null)
+        {
+            bulletWidth = collider.radius * 2;
+        }
+
+        for (int j = 0; j < simulNum; j++)
+        {
+            // 랜덤 높이
+            float height = Random.Range(-4.5f, 4.5f);
+
+            // 총알 시작 위치
+            Vector3 startPosition = transform.position + new Vector3(0, height, 0);
+
+            // 총알 생성
+            for (int i = 0; i < bulletCount; i++)
+            {
+                // 각 총알의 x 위치 계산
+                float xPos = startPosition.x + i * bulletWidth + j;
+
+                // 총알 생성
+                GameObject b = Instantiate(bullet7, new Vector3(xPos, height, 0), Quaternion.identity);
+                Rigidbody2D rigid = b.GetComponent<Rigidbody2D>();
+
+                rigid.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+            }
+        }
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
